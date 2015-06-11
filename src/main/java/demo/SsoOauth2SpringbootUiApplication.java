d114618d4e37ffafc8abf33f8c9789cba0dc408a
Giau Ngo <giaunv@gmail.com>
2015-06-11 11:59:45 +0700

    Setup

---
+++ src/main/java/demo/SsoOauth2SpringbootUiApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoOauth2SpringbootUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoOauth2SpringbootUiApplication.class, args);
+    }
+}
