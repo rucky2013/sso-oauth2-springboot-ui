a2a33bd129db52e70eb4fdaf551aecd567eb10b0
Giau Ngo <giaunv@gmail.com>
2015-06-13 06:22:06 +0700

    Sharing Authentication State

--- src/main/java/demo/SsoOauth2SpringbootUiApplication.java
+++ src/main/java/demo/SsoOauth2SpringbootUiApplication.java
@@ -3,6 +3,7 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import java.util.UUID;
 @SpringBootApplication
 @RestController
 @EnableZuulProxy
+@EnableRedisHttpSession
 public class SsoOauth2SpringbootUiApplication {
 
     @RequestMapping("/user")
